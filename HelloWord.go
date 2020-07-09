490f1a4fe70beff3c055b27fd021e36c9089e1f9
pratik wanjari <pratikwanjari96@gmail.com>
2020-07-09 20:34:43 +0530

    Create HelloWord.go

---
+++ HelloWord.go
@@ -0,0 +1,13 @@
+//program to make Report card of student
+package main // to indicate the main or program to execute
+
+//importing the required packages
+import(
+    "fmt" // Format package which contains the Println method
+)
+
+//main method
+func main(){
+    //Println method to print
+    fmt.Println("Hello World!")
+}
