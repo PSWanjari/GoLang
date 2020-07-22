69ccc22f0b24bdd0c1f923d30d68fa6f19888133
pratik wanjari <pratikwanjari96@gmail.com>
2020-07-22 10:57:16 +0530

    Update 18) EncapsulationMain.go

--- 18) EncapsulationMain.go
+++ 18) EncapsulationMain.go
@@ -8,17 +8,19 @@ import (
 
 func main() {
 	var todaysdate userdefinetype.Date
-	err := todaysdate.SetMonth(0)
+	//Setting the values ussing the setter mehod of the type
+	err := todaysdate.SetMonth(2)
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = todaysdate.SetDay(2)
+	err = todaysdate.SetDay(12)
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = todaysdate.SetYear(23)
+	err = todaysdate.SetYear(1996)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(todaysdate)
+	//getting the values using the getters
+	fmt.Printf("Day : %d\nMonth : %d\nYear : %d", todaysdate.Day(), todaysdate.Month(), todaysdate.Year())
 }
